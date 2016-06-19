62e43aa6a728df0e6086cc3209c78e6d460b5ae9
Aashish Vivekanand <aashishvanand@gmail.com>
2016-06-19 09:08:27 +0530

    Initial commit

---
+++ app/src/main/java/com/aashish/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,13 @@
+package com.aashish.stockhawk.touch_helper;
+
+import android.view.View;
+
+/**
+ * Created by sam_chordas on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface to enable swipe to delete
+ */
+public interface ItemTouchHelperAdapter {
+
+  void onItemDismiss(int position);
+}
